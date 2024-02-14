7e1e503ebeb820ea8cb77c3a7de8b124039b5345
daisukemiyoshi <nana40481023@gmail.com>
2024-02-14 10:43:00 +0900

    add: 周辺の座標の投稿のみ表示

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
+import 'dart:io';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:admob_flutter/admob_flutter.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:geolocator/geolocator.dart';
@@ -6,12 +8,14 @@ import 'firebase_options.dart';
 import 'position.dart';
 import 'database.dart';
 import 'image.dart';
+import 'services/admob.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
+  Admob.initialize();
   runApp(const MaterialApp(home: MyApp()));
 }
 
@@ -20,74 +24,128 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final service = DataService(); //データベースのクラス
+    return const MyPage();
+  }
+}
 
+class MyPage extends StatefulWidget {
+  const MyPage({Key? key}) : super(key: key);
+
+  @override
+  State<MyPage> createState() => _MyPage();
+}
+
+class _MyPage extends State<MyPage>{
+  QuerySnapshot? querySnapshotPosts;
+  List posts = [];
+  final service = DataService(); //データベースのクラス
+
+  Future<void> getPost2List() async {
+    querySnapshotPosts = await service.getPost();
+    Position position = await determinePosition();
+    posts.clear();
+    setState(() {
+      if (querySnapshotPosts != null){
+        //firestoreから来たデータを，扱いやすいようにmapが集まった，listにする
+        List tmpPosts = querySnapshotPosts!.docs.map((doc) => doc.data()).toList();
+        double distanceInMeters = 50;
+        for (Map<String, dynamic> tmpPost in tmpPosts){
+          if (distanceInMeters >= Geolocator.distanceBetween(position.latitude, position.longitude, tmpPost["coordinate"].latitude, tmpPost["coordinate"].longitude)) {
+            posts.add(tmpPost);
+          }
+        }
+      }
+      else {
+        posts.clear();
+      }
+    });
+  }
+
+  String getDiffDate(DateTime start, DateTime end) {
+  var dif = end.difference(start).inSeconds;
+
+  var hour = dif ~/ 60 ~/ 60;
+  var min = dif ~/ 60 % 60;
+  var sec = dif % 60;
+  return '$hour時間 $min分 $sec秒前';
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    // initState自体ではasync出来ないので，一度Futureに入れる
+    getPost2List();
+  }
+
+  @override
+  Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         body: Center(
-          child: Container(
-            constraints: const BoxConstraints(maxWidth: 400),
-            child: ListView.builder(
-              itemCount: _articles.length,
-              itemBuilder: (BuildContext context, int index) {
-                final item = _articles[index];
-                return Container(
-                  height: 136,
-                  margin:
-                      const EdgeInsets.symmetric(horizontal: 16, vertical: 8.0),
-                  decoration: BoxDecoration(
-                      border: Border.all(color: const Color(0xFFE0E0E0)),
-                      borderRadius: BorderRadius.circular(8.0)),
-                  padding: const EdgeInsets.all(8),
-                  child: Row(
-                    children: [
-                      Expanded(
-                          child: Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text(
-                            item.title,
-                            style: const TextStyle(fontWeight: FontWeight.bold),
-                            maxLines: 2,
-                            overflow: TextOverflow.ellipsis,
-                          ),
-                          const SizedBox(height: 8),
-                          Text("${item.author} · ${item.postedOn}",
-                              style: Theme.of(context).textTheme.caption),
-                          const SizedBox(height: 8),
-                          Row(
-                            mainAxisSize: MainAxisSize.min,
-                            children: [
-                              Icons.bookmark_border_rounded,
-                              Icons.share,
-                              Icons.more_vert
-                            ].map((e) {
-                              return InkWell(
-                                onTap: () {},
-                                child: Padding(
-                                  padding: const EdgeInsets.only(right: 8.0),
-                                  child: Icon(e, size: 16),
+          child: RefreshIndicator(
+            onRefresh: () async {
+              getPost2List();
+            },
+            child: Container(
+              constraints: const BoxConstraints(maxWidth: 400),
+              child: posts.isNotEmpty ? ListView.builder(
+                itemCount: posts.length,
+                itemBuilder: (BuildContext context, int index) {
+                  final item = posts[index];
+                  return Container(
+                    height: 136,
+                    margin:
+                        const EdgeInsets.symmetric(horizontal: 16, vertical: 8.0),
+                    decoration: BoxDecoration(
+                        border: Border.all(color: const Color(0xFFE0E0E0)),
+                        borderRadius: BorderRadius.circular(8.0)),
+                    padding: const EdgeInsets.all(8),
+                    child: Row(
+                      children: [
+                        Expanded(
+                            child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              item["title"],
+                              style: const TextStyle(fontWeight: FontWeight.bold),
+                              maxLines: 2,
+                              overflow: TextOverflow.ellipsis,
+                            ),
+                            const SizedBox(height: 8),
+                            Text(
+                              getDiffDate(item["createdAt"].toDate(), DateTime.now()),
+                              style: Theme.of(context).textTheme.bodySmall!.merge(
+                                const TextStyle(
+                                  color: Colors.grey,
                                 ),
-                              );
-                            }).toList(),
-                          )
-                        ],
-                      )),
-                      Container(
-                          width: 100,
-                          height: 100,
-                          decoration: BoxDecoration(
-                              color: Colors.grey,
-                              borderRadius: BorderRadius.circular(8.0),
-                              image: DecorationImage(
-                                fit: BoxFit.cover,
-                                image: NetworkImage(item.imageUrl),
-                              ))),
-                    ],
-                  ),
-                );
-              },
+                              ),
+                            ),
+                            const SizedBox(height: 8),
+                            Text(item["content"],
+                                style: Theme.of(context).textTheme.bodySmall,
+                                maxLines: 3,
+                                overflow: TextOverflow.ellipsis,
+                              ),
+                            const SizedBox(height: 8),
+                          ],
+                        )),
+                        Container(
+                            width: 100,
+                            height: 100,
+                            decoration: BoxDecoration(
+                                color: Colors.grey,
+                                borderRadius: BorderRadius.circular(8.0),
+                                image: DecorationImage(
+                                  fit: BoxFit.cover,
+                                  image: NetworkImage(item["imageUrl"]),
+                                ))),
+                      ],
+                    ),
+                  );
+                },
+              ) : ListView(), //何もないときもスクロールで更新できるように，空のListViewを追加
             ),
           ),
         ),
@@ -96,24 +154,41 @@ class MyApp extends StatelessWidget {
             showDialog<void>(
               context: context,
               builder: (_) {
-                return const AlertDialogSample();
+                return AlertDialogSample(getPost2List: getPost2List);
               });
           },
           tooltip: 'Increment',
           child: const Icon(Icons.add),
-        ), // This trailing comma makes auto-formatting nicer for build methods.
+        ),
+        bottomNavigationBar: Column(
+        mainAxisAlignment: MainAxisAlignment.end,
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          AdmobBanner(
+            adUnitId: AdMobService().getBannerAdUnitId(),
+            adSize: AdmobBannerSize(
+              width: MediaQuery.of(context).size.width.toInt(),
+              height: AdMobService().getHeight(context).toInt(),
+              name: 'SMART_BANNER',
+            ),
+          ),
+        ],
+      ),
       ),
     );
   }
 }
 
 class AlertDialogSample extends StatelessWidget {
-  const AlertDialogSample({Key? key}) : super(key: key);
+  final VoidCallback getPost2List;
+  const AlertDialogSample({Key? key, required this.getPost2List}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     final service = DataService(); //データベースのクラス
     var contentController = TextEditingController();
+    File? image;
+    String imageUrl = "https://www.shoshinsha-design.com/wp-content/uploads/2020/05/noimage-760x460.png";
     String title = "ポケカ";
 
     return AlertDialog(
@@ -125,26 +200,48 @@ class AlertDialogSample extends StatelessWidget {
       content: TextField(
         keyboardType: TextInputType.multiline,
         controller: contentController,
-        maxLines: null,
+        maxLines: 3,
+        maxLength: 100,
         decoration: const InputDecoration(
           labelText: "備考",
-          // hintText: "Some Hint"
         ),         
       ),
       actions: <Widget>[
         FloatingActionButton.extended(
-          onPressed: () {  
-            getImageFromCamera();
+          onPressed: () async {  
+            image = await getImageFromCamera();
           },
           icon: const Icon(Icons.add),
           label: const Text("写真を追加する"), 
           ),
         FloatingActionButton.extended(
           onPressed: () async {
+            // ロード画面が表示
+            showDialog<void>(
+              context: context,
+              barrierDismissible: false,
+              builder: (_) => const Center(child: CircularProgressIndicator()),
+            );
+            
             // データベースに登録
             Position position = await determinePosition();
             GeoPoint coordinate = GeoPoint(position.latitude, position.longitude);
-            service.addPost(title, contentController.text, coordinate, "https://firebasestorage.googleapis.com/v0/b/opponent-matching.appspot.com/o/images%2Fgaryu.jpg?alt=media&token=57609fd0-1867-4279-8c4e-01c249d351ce");
+
+            // 画像が登録されてる時は，画像をStorageに保存して，URLを取ってくる
+            if (image != null){
+              imageUrl = await registerImage4Storage(image!); 
+            }
+
+            // 投稿内容をfirestoreに登録
+            service.addPost(title, contentController.text, coordinate, imageUrl);
+
+            getPost2List();
+            
+            // 非同期の中でcontextを使うときの対策
+            if (!context.mounted) return;
+            // ロード画面とアラートダイアログを消す
+            Navigator.of(context).pop();
+            Navigator.of(context).pop();
           },
           label: const Text("投稿"),
         ),
@@ -202,62 +299,3 @@ class _DropdownButtonMenuState extends State<DropdownButtonMenu> {
     );
   }
 }
-
-class Article {
-  final String title;
-  final String imageUrl;
-  final String author;
-  final String postedOn;
-
-  Article(
-      {required this.title,
-      required this.imageUrl,
-      required this.author,
-      required this.postedOn});
-}
-
-final List<Article> _articles = [
-  Article(
-    title: "デュエマ",
-    author: "MacRumors",
-    imageUrl: "https://firebasestorage.googleapis.com/v0/b/opponent-matching.appspot.com/o/images%2Fgaryu.jpg?alt=media&token=57609fd0-1867-4279-8c4e-01c249d351ce",
-    postedOn: "Yesterday",
-  ),
-  Article(
-      title: "ポケカ",
-      imageUrl: "",
-      author: "9to5Google",
-      postedOn: "4 hours ago"),
-  Article(
-    title: "シャドバ",
-    author: "New York Times",
-    imageUrl: "https://pbs.twimg.com/media/F_dSvkca0AA0hWq?format=jpg&name=900x900",
-    postedOn: "2 days ago",
-  ),
-  Article(
-    title:
-        "Amazon’s incredibly popular Lost Ark MMO is ‘at capacity’ in central Europe",
-    author: "MacRumors",
-    imageUrl: "https://picsum.photos/id/1002/960/540",
-    postedOn: "22 hours ago",
-  ),
-  Article(
-    title:
-        "Panasonic's 25-megapixel GH6 is the highest resolution Micro Four Thirds camera yet",
-    author: "Polygon",
-    imageUrl: "https://picsum.photos/id/1020/960/540",
-    postedOn: "2 hours ago",
-  ),
-  Article(
-    title: "Samsung Galaxy S22 Ultra charges strangely slowly",
-    author: "TechRadar",
-    imageUrl: "https://picsum.photos/id/1021/960/540",
-    postedOn: "10 days ago",
-  ),
-  Article(
-    title: "Snapchat unveils real-time location sharing",
-    author: "Fox Business",
-    imageUrl: "https://picsum.photos/id/1060/960/540",
-    postedOn: "10 hours ago",
-  ),
-];
