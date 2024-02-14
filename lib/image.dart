431026e337d63a900550a9f51e83713e53ab071b
daisukemiyoshi <nana40481023@gmail.com>
2024-02-14 10:41:33 +0900

    add: storageに画像登録，削除機能実装

--- lib/image.dart
+++ lib/image.dart
@@ -1,6 +1,7 @@
 import 'dart:io';
 import 'package:image_picker/image_picker.dart';
 import 'package:firebase_storage/firebase_storage.dart';
+import 'package:uuid/uuid.dart';
 
 Future getImageFromCamera() async {
   File? image;
@@ -8,13 +9,28 @@ Future getImageFromCamera() async {
   final pickedFile = await picker.pickImage(source: ImageSource.camera);
 
   if (pickedFile != null){
-    print("aa");
     image = File(pickedFile.path);
-    final storageRef = FirebaseStorage.instance.ref("/images").child("/garyu.jpg");
-    final task = await storageRef.putFile(image);
-    print(await storageRef.getDownloadURL());
+
+    return image;
   }
   else {
-    print("画像が撮影されませんでした．");
+    return 0;
   }
  }
+
+ Future registerImage4Storage(File image) async {
+  const uuid = Uuid();
+
+  String newUuid = uuid.v4();
+  final storageRef = FirebaseStorage.instance.ref("/images").child("/$newUuid.jpg");
+  await storageRef.putFile(image);
+
+  return await storageRef.getDownloadURL();
+ }
+// urlからpathを取得できる
+// storageRef = FirebaseStorage.instance.refFromURL(url)
+
+Future deleteImage4Storage(String imageUrl) async {
+final storageRef = FirebaseStorage.instance.refFromURL(imageUrl);
+await storageRef.delete();
+}
