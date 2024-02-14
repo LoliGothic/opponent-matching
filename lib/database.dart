4743f0b640d77df15c8395795539361f888a703e
daisukemiyoshi <nana40481023@gmail.com>
2024-02-14 10:42:15 +0900

    add: 投稿削除機能実装

--- lib/database.dart
+++ lib/database.dart
@@ -5,13 +5,27 @@ class DataService {
   // データベースの参照を取得
   final db = FirebaseFirestore.instance;
   // データベースにデータを追加
-  Future<void> addPost(String title, String content, GeoPoint coordinate, String imagePath) {
+  Future<void> addPost(String title, String content, GeoPoint coordinate, String imageUrl) {
     return db.collection('posts').add({
       'title': title,
       'content': content,
       'coordinate': coordinate,
-      'imagePath': imagePath,
+      'imageUrl': imageUrl,
       'createdAt': Timestamp.now(),
     });
+  }
+
+  Future getPost() {
+    return db.collection('posts').orderBy('createdAt', descending: true).get();
+  }
+
+  Future<void> deletePost() async {
+    DateTime tmpSixHoursAgo = DateTime.now().subtract(const Duration(minutes: 1)); //6時間前の時刻を取得
+    Timestamp sixHoursAgo = Timestamp.fromDate(tmpSixHoursAgo); //timestamp型に変更
+    QuerySnapshot snapshot = await db.collection('posts').where('createdAt', isLessThan: sixHoursAgo).get(); //全てを取得
+    
+    for (QueryDocumentSnapshot doc in snapshot.docs){
+      await doc.reference.delete();
+    }
   }
 }
